8afbf4d4d2a4aad4fd767719374f0ba609fbeec9
ViniciusLima99 <vinicius.lima9910@gmail.com>
2025-01-15 16:20:59 -0300

    Delete src directory

--- src/main/java/br/com/testeSpring/Exceptions/CriptoExistException.java
+++
@@ -1,9 +0,0 @@
-package br.com.testeSpring.Exceptions;
-
-public class CriptoExistException extends Exception {
-    public CriptoExistException(String message) {
-        super(message);
-    }
-    private static final long serialVersionUID = 1L;
-
-}
--- src/main/java/br/com/testeSpring/Exceptions/ServiceException2.java
+++
@@ -1,8 +0,0 @@
-package br.com.testeSpring.Exceptions;
-
-public class ServiceException2 extends Exception {
-    public ServiceException2(String message) {
-        super(message);
-    }
-    private static final long serialVersionUID = 1L;
-}
